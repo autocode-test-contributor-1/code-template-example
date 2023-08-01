c4f276629d64e492444b786ba79ccb93a1564599
autocode-test-contributor-1 <70589511+autocode-test-contributor-1@users.noreply.github.com>
2023-08-01 14:38:21 +0000

    solution-commit-contributor-1

--- src/main/java/org/example/Template.java
+++ src/main/java/org/example/Template.java
@@ -3,18 +3,7 @@ package org.example;
 public class Template {
     public String reverseString(String str) {
         StringBuilder builder = new StringBuilder(str);
-        return builder.reverse().toString();
+        return new StringBuilder(str).reverse().toString();
     }
 
-    public static boolean isPalindrome(int x) {
-        var intToString = String.valueOf(x);
-        for (int i = 0; i < intToString.length() - 1; i++) {
-            if (intToString.charAt(i) != intToString.charAt(intToString.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //feature
 }
